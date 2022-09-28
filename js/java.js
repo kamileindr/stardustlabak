09e79a2c57d6afab011761b584b8aec3d623e57a
kamileindr <kami8182@stud.kea.dk>
2022-09-28 23:41:06 +0100

    almost done landing

--- js/java.js
+++ js/java.js
@@ -1,4 +1,4 @@
-const url = "https://kea22-38f9.restdb.io/rest/stardust?max=20"
+const url = "https://kea22-38f9.restdb.io/rest/stardust?max=3";
 
 const options = {
     headers: {
@@ -14,8 +14,27 @@ fetch(url, options)
         return response.json();
     })
     .then((data) => {
-        console.log(data);
+        //console.log(data);
+        handleData(data);
     })
     .catch((e) => {
         console.error("An error occured:", e.message);
     });
+
+function handleData(stardust){
+    stardust.forEach((item) => {
+        console.log(item);
+        // make a template
+        //grab it
+        const template = document.querySelector("template").content;
+        //clone it
+        const clone = template.cloneNode(true);
+        //populate with data
+        clone.querySelector("img").src = item.img_url;
+        clone.querySelector("h2").textContent = item.name;
+        clone.querySelector("h3").textContent = item.price;
+        //append it to the dom
+        const mainEl = document.querySelector("main");
+        mainEl.appendChild(clone);
+    });
+}
